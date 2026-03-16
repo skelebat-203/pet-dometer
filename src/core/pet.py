e04b089016d11af91c67209418c51594d9001a51
skelebat-203 <260954653+skelebat-203@users.noreply.github.com>
2026-03-16 12:49:10 -0400

    Put in - add_resources() - getter / setters - added Annotated so varable descriptions can be read across files.

--- src/core/pet.py
+++ src/core/pet.py
@@ -1,66 +1,81 @@
 '''Pet state, status, etc.'''
+from typing import Annotated
 
 class Pet:
     MOOD_LIST = ["excited", "happy", "fine", "upset", "mad"]
-    MAX_PROGRESS_CHANGE = 0.10 # Max up / down progress change 
+    MAX_PROGRESS_CHANGE: Annotated[float, "Max up / down progress change"] = 0.10
+    UNITS_PER_RANK = 100
+    CUSHION: Annotated[float, "Smooths out progress we add a 'Cushion' whne you move up/down a rank"] = 0.05
+    PET_STARTING_PROGRESS: Annotated[int, "When a new pet instance is created set progress to 0"] = 0
     #
     #  Consider adding a modifier for each mood level
     #
-    default_progress = 0
     def __init__(self):
-        self.resource = 0 # used to note how many resources the pet has received today
-        self.mood_index = 2 # this is how the pet feels about you
-        self.progress = Pet.default_progress # this notes the progress to the next mood level, updates contuniously
-        self.resources_received_today = 0 # logs resources received today; resets each day
+        self.resource: Annotated[int, "used to note how many resources the pet has received today"] = 0
+        self.mood_index: Annotated[int, "this is how the pet feels about you"] = 2
+        self.progress: Annotated[int, "his notes the progress to the next mood level, updates contuniously"] = Pet.PET_STARTING_PROGRESS 
+        self.resources_received_today: Annotated[int, "logs resources received today; resets each day"] = 0
     
-    '''Getters / Setters'''
+    # Getters
     def get_current_mood(self):
         '''get pet mood'''
-        return MOOD_LIST[self.mood_index]
-    def get_current_progress(self):
+        return self.MOOD_LIST[self.mood_index]
+    def get_progress(self):
         '''get progress to new mood'''
         return self.progress
+    def get_resources_received_today(self):
+        '''get current resources added today'''
+        return self.get_resources_received_today
 
-    def set_gift_resources(self, n_resources:int):
-        '''player gift resources to pet
-        return: resources
-        '''
-        
-        self.resources_received_today = n_resources
-        self.resource = self.resource + n_resources
-
-        temp_prog = self.progress + n_resources
-
-        if (n_resources + self.resource) > 100: 
-            print("\ngo up")
-            self.mood_index = self.mood_index + 1
-
-            if temp_prog % 100 == 0: self.progress = 5
-            else: self.progress = self.progress + n_resources - 100
-
-        elif (n_resources + self.resource) < 0: 
-            print("\ndrop")
-            self.mood_index = self.mood_index - 1
-
-            if temp_prog % 100 == 0: self.progress = 95
-            else: self.progress = self.progress + n_resources + 100
-            
-        else: 
-            print("\nstay")
-            self.progress = self.progress + n_resources
-
-        return self.resource, self.mood_index, self.resources_received_today
-
-    def set_walk_resources(self, n_resource:int):
-        '''Accept resources
-        return: resources
-        '''
-        return self.resource + n_resource
+    # Setters
+    def set_mood(self, mood:int):
+        self.mood_index = mood
+    def set_progress(self, progress:int):
+        self.progress = progress
+    def set_resources_received_today(self, resources_received_today:int):
+        self.resources_received_today = resources_received_today
+    def set_resource(self, resource:int):
+        self.resource = resource
 
     #hooks
     def new_day(self):
+        '''Reset `resources_received_today` to 0
+        
+        Called by file that tracks date/time.'''
         self.resources_received_today = 0
 
+    #Other Methods
+    def add_resources(self, n_resources:int):
+        '''Add resources to pet. Updates values:
+            - resources_received_today
+            - resource
+            - mood_index
+            - progress
+        '''
+        self.set_resources_received_today(self.resources_received_today + n_resources)
+        self.set_resource(self.resource + n_resources)
+
+        temp_prog = self.get_progress() + n_resources
+        
+        if (n_resources + self.resource) > self.UNITS_PER_RANK: # increase mood / update progress
+            self.set_mood(self.mood_index + 1)
+            
+            # Update progress
+            if temp_prog % self.UNITS_PER_RANK == 0: 
+                self.set_progress(int(self.UNITS_PER_RANK * self.CUSHION))
+            else: self.set_progress(self.progress + n_resources - self.UNITS_PER_RANK)
+
+        elif (n_resources + self.resource) < 0:  # decrease mood / update progress
+            self.set_mood(self.mood_index - 1)
+
+            # Update progress
+            if temp_prog % self.UNITS_PER_RANK == 0: 
+                self.set_progress(int(self.UNITS_PER_RANK * (1-self.CUSHION)))
+            else: self.set_progress(self.progress + n_resources + self.UNITS_PER_RANK)
+            
+        else:  # nomood change / update progress
+            self.set_progress(self.progress + n_resources)
+    
 pet = Pet()
 
 # test logic
@@ -69,10 +84,11 @@ temp_progresss = pet.progress
 temp_resources = pet.resource
 temp_resources_received_today = pet.resources_received_today
 
-pet.set_gift_resources(45)
+change = -100
+pet.add_resources(change)
 
-print(f"-50 resources")
-print(f"Mood Index: \n Original: {temp_mood_index}\tNew: {pet.mood_index}")
+print(f"{change} resources")
+print(f"Mood Index: \n Original: {temp_mood_index}\tNew: {pet.mood_index} '{pet.get_current_mood()}'")
 print(f"Progress: \n Original: {temp_progresss}\tNew: {pet.progress}")
 print(f"Current resources: \n Original: {temp_resources}\tNew: {pet.resource}")
 print(f"Resources today: \n Original: {temp_resources_received_today}\tNew: {pet.resources_received_today}")
