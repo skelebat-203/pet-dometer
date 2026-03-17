9226682d6d7bbad2fe392c6926bbe5475050b39b
skelebat-203 <260954653+skelebat-203@users.noreply.github.com>
2026-03-17 07:23:14 -0400

    moved resource subtraction into it's own method

--- src/core/pet.py
+++ src/core/pet.py
@@ -2,9 +2,9 @@
 from typing import Annotated
 
 class Pet:
-    MOOD_LIST = ["excited", "happy", "fine", "upset", "mad"]
+    MOOD_LIST = ["mad", "upset", "fine", "happy", "excited"]
     MAX_PROGRESS_CHANGE: Annotated[float, "Max up / down progress change"] = 0.10
-    UNITS_PER_RANK = 100
+    UNITS_PER_RANK = 200
     CUSHION: Annotated[float, "Smooths out progress we add a 'Cushion' whne you move up/down a rank"] = 0.05
     PET_STARTING_PROGRESS: Annotated[int, "When a new pet instance is created set progress to 0"] = 0
     #
@@ -16,6 +16,14 @@ class Pet:
         self.progress: Annotated[int, "his notes the progress to the next mood level, updates contuniously"] = Pet.PET_STARTING_PROGRESS 
         self.resources_received_today: Annotated[int, "logs resources received today; resets each day"] = 0
     
+
+    def test_output(self, change:int):
+        print(f"{change} resources")
+        print(f"Mood Index: \n Original: {temp_mood_index}\tNew: {pet.mood_index} '{pet.get_current_mood()}'")
+        print(f"Progress: \n Original: {temp_progresss}\tNew: {pet.progress}")
+        print(f"Current resources: \n Original: {temp_resources}\tNew: {pet.resource}")
+        print(f"Resources today: \n Original: {temp_resources_received_today}\tNew: {pet.resources_received_today}")
+
     # Getters
     def get_current_mood(self):
         '''get pet mood'''
@@ -64,8 +72,26 @@ class Pet:
             if temp_prog % self.UNITS_PER_RANK == 0: 
                 self.set_progress(int(self.UNITS_PER_RANK * self.CUSHION))
             else: self.set_progress(self.progress + n_resources - self.UNITS_PER_RANK)
+            
+        else:  # nomood change / update progress
+            self.set_progress(self.progress + n_resources)
 
-        elif (n_resources + self.resource) < 0:  # decrease mood / update progress
+        print("\nADD RESOURCES")
+        self.test_output(n_resources)
+    
+    def remove_resources(self, n_resources:int):
+        '''Remove resources to pet. Updates values:
+            - resources_received_today
+            - resource
+            - mood_index
+            - progress
+        '''
+        self.set_resources_received_today(self.resources_received_today + n_resources)
+        self.set_resource(self.resource + n_resources)
+
+        temp_prog = self.get_progress() + n_resources
+        
+        if (n_resources + self.resource) < 0:  # decrease mood / update progress
             self.set_mood(self.mood_index - 1)
 
             # Update progress
@@ -75,7 +101,11 @@ class Pet:
             
         else:  # nomood change / update progress
             self.set_progress(self.progress + n_resources)
-    
+
+        print("\nREMOVE RESOURCES")
+        self.test_output(n_resources)
+
+
 pet = Pet()
 
 # test logic
@@ -84,16 +114,8 @@ temp_progresss = pet.progress
 temp_resources = pet.resource
 temp_resources_received_today = pet.resources_received_today
 
-change = -100
-pet.add_resources(change)
+add = 140
+# pet.add_resources(add)
+remove = -90
+pet.remove_resources(remove)
 
-print(f"{change} resources")
-print(f"Mood Index: \n Original: {temp_mood_index}\tNew: {pet.mood_index} '{pet.get_current_mood()}'")
-print(f"Progress: \n Original: {temp_progresss}\tNew: {pet.progress}")
-print(f"Current resources: \n Original: {temp_resources}\tNew: {pet.resource}")
-print(f"Resources today: \n Original: {temp_resources_received_today}\tNew: {pet.resources_received_today}")
-
-pet.mood_index = temp_mood_index
-pet.progress = temp_progresss
-pet.resource = temp_resources
-pet.resources_received_today = temp_resources_received_today
